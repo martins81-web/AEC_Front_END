55471fda01dfd927f06c28d5ac076c946773d144
martins81-web <ericmartins81@gmail.com>
2020-03-19 20:37:46 -0400

    update

---
+++ Programmation1/Casse-Tete/JS/casseTete.js
@@ -0,0 +1,29 @@
+
+var imgClicke=false;
+var imgID1;
+var imgSrc2;
+
+$('.img-fluid').on('click', function() {
+    if (imgClicke===false) {
+        imgID1 = $(this);
+        imgClicke=true;
+        imgID1.addClass('border border-danger');
+    } else{
+        imgSrc2 = $(this).attr('src');
+        $(this).attr("src", imgID1.attr("src"));
+        imgID1.attr("src", imgSrc2);
+        imgClicke=false;
+        imgID1.removeClass('border border-danger');
+    }
+});
+
+
+$('#boutonRecommencer').click(function() {
+    location.reload();
+});
+
+$('#boutonTricher').click(function() {
+    for (let i=0; i<9; i++){
+        $("#image"+i).attr("src","images/"+i+".jpg");
+    }
+});
