c71be9fed30e4270d67e23cb82b238a3bc135f61
martins81-web <ericmartins81@gmail.com>
2020-03-19 22:44:49 -0400

    update

--- Programmation1/Casse-Tete/JS/casseTete.js
+++ Programmation1/Casse-Tete/JS/casseTete.js
@@ -1,24 +1,33 @@
 
-var imgClicke=false;
+var premierClique=true;
 var $img1;
 var $img2;
 var $imgSrc1;
 var $imgSrc2;
+var coups=0;
 
 $('.img-fluid').on('click', function() {
-    if (imgClicke===false) {
+    if (premierClique===true) {
         $img1 = $(this);
         $imgSrc1 = $(this).attr('src');
-        $img1.addClass('border border-danger');
-        imgClicke=true;
+        $img1.hide();
+        premierClique=false;
     } else{
         $img2 = $(this);
         $imgSrc2 = $(this).attr('src');
         $img2.attr("src", $imgSrc1);
         $img1.attr("src", $imgSrc2);
-        $img1.removeClass('border border-danger');
-        imgClicke=false;
+        $img1.show();
+        premierClique=true;
+        coups++;
+
+        if (Victoire()){
+            addDivVictoire();
+            addCoups();
+            $("#boutonTricher").attr("disabled", true);
+        }
     }
+
 });
 
 
@@ -30,4 +39,46 @@ $('#boutonTricher').click(function() {
     for (let i=1; i<=9; i++){
         $("#image"+i).attr("src","images/"+i+".jpg");
     }
+    addDivVictoire();
+    addTricheur();
+    $("#boutonTricher").attr("disabled", true);
 });
+
+
+$("img").hover(function() {
+    $(this).css('cursor','pointer');
+});
+
+
+/**
+ * @return {boolean}
+ */
+function Victoire(){
+    let compteur=0;
+    for (let i=1; i<=9; i++){
+        let id = $("#image"+i).attr("id");
+        let src= $("#image"+i).attr("src");
+        if (retnum(id)===retnum(src)){
+            compteur++;
+        }
+    }
+    return compteur === 9;
+}
+
+
+function retnum(str) {
+    var num = str.replace(/[^0-9]/g, '');
+    return parseInt(num,10);
+}
+
+function addDivVictoire(){
+$('#premiereRangee').before('<div id="victoire"><img class="img-fluid" src="images/victoire.jpg"/></div>');
+}
+
+function addCoups(){
+    $('#victoire').append("<h1>"+coups+" coups ont été necéssaires pour résoudre le casse-tête.</h1>");
+}
+
+function addTricheur(){
+    $('#victoire').append("<h1>TRICHEUR</h1>");
+}
