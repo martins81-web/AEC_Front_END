bdb9b71c523fba86dd0f2ad9211f316084a11059
martins81-web <ericmartins81@gmail.com>
2020-03-19 21:00:19 -0400

    update

--- Programmation1/Casse-Tete/JS/casseTete.js
+++ Programmation1/Casse-Tete/JS/casseTete.js
@@ -1,19 +1,23 @@
 
 var imgClicke=false;
-var imgID1;
-var imgSrc2;
+var $img1;
+var $img2;
+var $imgSrc1;
+var $imgSrc2;
 
 $('.img-fluid').on('click', function() {
     if (imgClicke===false) {
-        imgID1 = $(this);
+        $img1 = $(this);
+        $imgSrc1 = $(this).attr('src');
+        $img1.addClass('border border-danger');
         imgClicke=true;
-        imgID1.addClass('border border-danger');
     } else{
-        imgSrc2 = $(this).attr('src');
-        $(this).attr("src", imgID1.attr("src"));
-        imgID1.attr("src", imgSrc2);
+        $img2 = $(this);
+        $imgSrc2 = $(this).attr('src');
+        $img2.attr("src", $imgSrc1);
+        $img1.attr("src", $imgSrc2);
+        $img1.removeClass('border border-danger');
         imgClicke=false;
-        imgID1.removeClass('border border-danger');
     }
 });
 
