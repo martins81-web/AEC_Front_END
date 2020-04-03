f46e5b58bbb5b0150e6370dc98af915475939c64
martins81-web <ericmartins81@gmail.com>
2020-04-02 20:40:08 -0400

    update

--- Integration1/Projet final/JS/portugal.js
+++ Integration1/Projet final/JS/portugal.js
@@ -1,13 +1,5 @@
 
-var premierClique=true;
 
-$( ".custom-toggler" ).click(function() {
-    if (premierClique===true) {
-        $(".custom-toggler").addClass("rotated");
-        premierClique=false;
-    }
-    else{
-        $(".custom-toggler").removeClass("rotated");
-        premierClique=true;
-    }
+$( "#hamb" ).click(function() {
+        $("#arrowHamb").toggleClass("rotated");
 });
