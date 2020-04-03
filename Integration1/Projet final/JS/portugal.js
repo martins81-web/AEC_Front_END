bb019922df0556e82303c383274a4beb69b973f9
martins81-web <ericmartins81@gmail.com>
2020-04-02 20:53:51 -0400

    update

--- Integration1/Projet final/JS/portugal.js
+++ Integration1/Projet final/JS/portugal.js
@@ -3,3 +3,17 @@
 $( "#hamb" ).click(function() {
         $("#arrowHamb").toggleClass("rotated");
 });
+
+
+
+$(document).on('mouseenter', '#btnPhotos', function(e) {
+        $(this).css('background-color', "white");
+        $(this).css('color', '#050043');
+        $(this).css('border', '1px solid black');
+});
+
+$(document).on('mouseleave', '#btnPhotos', function(e) {
+        $(this).css('color', 'white');
+        $(this).css('background-color', "#050043");
+});
+
