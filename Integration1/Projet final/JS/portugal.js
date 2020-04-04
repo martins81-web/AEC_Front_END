a249ca2a710c9b72c5ce9e2cee5a84123d509ad6
martins81-web <ericmartins81@gmail.com>
2020-04-04 10:29:16 -0400

    update

--- Integration1/Projet final/JS/portugal.js
+++ Integration1/Projet final/JS/portugal.js
@@ -1,12 +1,16 @@
+<!-- portugal.js -->
+<!-- Projet final d'Integration1 par Eric Martins -->
+<!-- Crée le 12 mars 2020 -->
+<!-- Dernier mise à jour le 04 avril 2020 -->
 
-
-
+/* fait la rotation de l'image de l'hamburguer custom toggler dans le navbar */
 $( ".navbar-toggler" ).click(function() {
         $("#arrowHamb").toggleClass("rotated");
 });
 
 
-
+/* Change la couleur du bouton en passand le souris dessus*/
+/* bouton premier page section 2*/
 $(document).on('mouseenter', '#btnPhotos', function(e) {
         $(this).css('background-color', "white");
         $(this).css('color', '#050043');
@@ -19,30 +23,22 @@ $(document).on('mouseleave', '#btnPhotos', function(e) {
 });
 
 
-
+/*Musique en cliquant sur les drapeaux - EASTER EGG*/
 var himne = new Audio();
 himne.src = "https://upload.wikimedia.org/wikipedia/commons/5/58/A_Portuguesa.ogg";
 
 var click=true;
 $(document).on('click', "#drapeauHeader, #drapeauFooter", function(e){
         if (click===true){
-                PlaySound();
+                himne.play();
                 click=false;
 
         } else {
-                PauseSound();
+                himne.pause();
                 click=true;
         }
 });
 
-
-function PlaySound() {
-        himne.play();
-}
-
-function PauseSound() {
-        himne.pause();
-}
 
 
 
