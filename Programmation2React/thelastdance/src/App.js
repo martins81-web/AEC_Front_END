ce383543748bd3542fac3eb19ce435be3e33989e
martins81-web <ericmartins81@gmail.com>
2020-04-21 21:42:36 -0400

    Premier commit React

--- Programmation2React/thelastdance/src/App.js
+++ Programmation2React/thelastdance/src/App.js
@@ -1,24 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import logoNetflix from './Img/logo netflix.png';
+import imgMJ from './Img/MJ.jpg';
+import imgCB from './Img/CB.jpg';
+import imgLB from './Img/LB.jpg';
+import imgSP from './Img/SP.jpg';
+import imgMagic from './Img/Magic.jpg';
+
+const joueurs = ['Michael Jordan', 'Larry Bird', 'Charles Barkley', 'Magic Johnson', 'Scottie Pippen'];
+let random= Math.floor(Math.random() * 5); 
+const PhotosJoueurs= [imgMJ,imgLB,imgCB,imgMagic,imgSP];
+
+const partants = joueurs.map(element => <li>{element}</li>);
+const partantsAffiches =joueurs.map(element => joueurs[random] !== element && <li> {element}</li> );
+
+const photo = (  
+  <img   
+    src={PhotosJoueurs[random]}
+    alt={joueurs[random]}   
+    height={350} />  
+); 
+
+var nomJoueurAffiche= joueurs[random];
 
 function App() {
+  var texteAfficher=(<h1>THE LAST DANCE</h1>);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {texteAfficher}
+      <p>Une production de</p>
+      <img src={logoNetflix} alt="logo" height="150px" />
+      <p>Mettant en vedette</p>
+      <h2>{nomJoueurAffiche}</h2>
+      {photo}
+      
+      <h3>Le 5 partants</h3>
+      <ul>{partants}</ul>
+      
+      <h3>Le 4 partants affichés</h3>
+      <ul>{partantsAffiches}</ul>
     </div>
   );
 }
