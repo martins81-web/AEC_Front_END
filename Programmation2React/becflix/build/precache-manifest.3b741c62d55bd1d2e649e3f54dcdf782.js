f808ce0fb940892674314875b7c79efdd8c4c58c
martins81-web <ericmartins81@gmail.com>
2020-04-28 19:07:46 -0400

    becflix

---
+++ Programmation2React/becflix/build/precache-manifest.3b741c62d55bd1d2e649e3f54dcdf782.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "67d2d2d6e58869b73d3848d2d7be9f56",
+    "url": "./index.html"
+  },
+  {
+    "revision": "78ac44106968bd7df5c8",
+    "url": "./static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "3f3ddf78899f0356986d",
+    "url": "./static/js/2.36b9ad29.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.36b9ad29.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78ac44106968bd7df5c8",
+    "url": "./static/js/main.967988c5.chunk.js"
+  },
+  {
+    "revision": "ef8e272eb776f0edacee",
+    "url": "./static/js/runtime-main.03e9e90d.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "./static/media/logo.5d5d9eef.svg"
+  }
+]);
