4ea6c2b88f1e9c5bae6f06b760ce74c1c7504ab4
martins81-web <ericmartins81@gmail.com>
2020-06-24 00:16:05 -0400

    upload rebrandly

---
+++ Programmation2React/rebrandly/build/precache-manifest.fe4d3ceb8eba5c74cee9901f56f73af6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b18a0622cb586f1145ddc6ba00dedaf9",
+    "url": "./index.html"
+  },
+  {
+    "revision": "b3d9e5f0616c6b2b36ee",
+    "url": "./static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "314f8f31525e40555b61",
+    "url": "./static/css/main.1ba354e9.chunk.css"
+  },
+  {
+    "revision": "b3d9e5f0616c6b2b36ee",
+    "url": "./static/js/2.1ce423bb.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "./static/js/2.1ce423bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "314f8f31525e40555b61",
+    "url": "./static/js/main.62a89b72.chunk.js"
+  },
+  {
+    "revision": "bf121f99ce1dafbc798b",
+    "url": "./static/js/runtime-main.e1da497a.js"
+  }
+]);
