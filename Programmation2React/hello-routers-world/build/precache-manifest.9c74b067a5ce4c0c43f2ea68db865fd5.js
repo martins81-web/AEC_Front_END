efebae83f0c2436922b47e5fc26b951c892aaef3
martins81-web <ericmartins81@gmail.com>
2020-07-01 00:13:48 -0400

    update hello router

---
+++ Programmation2React/hello-routers-world/build/precache-manifest.9c74b067a5ce4c0c43f2ea68db865fd5.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eba3590d3f8223cbb78691aca5d56528",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9eabae9d72c88e3362d7",
+    "url": "./static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "58151d7eabe06cf5d35a",
+    "url": "./static/css/main.d0e34979.chunk.css"
+  },
+  {
+    "revision": "9eabae9d72c88e3362d7",
+    "url": "./static/js/2.5d0a6dc6.chunk.js"
+  },
+  {
+    "revision": "d3eab2c800484648f446253871549a7a",
+    "url": "./static/js/2.5d0a6dc6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "58151d7eabe06cf5d35a",
+    "url": "./static/js/main.fd0c8e15.chunk.js"
+  },
+  {
+    "revision": "2690094dade02caa8aeb",
+    "url": "./static/js/runtime-main.a18ec8eb.js"
+  },
+  {
+    "revision": "18a2ae6f4456a69e74e023669c6aa737",
+    "url": "./static/media/Bruce.18a2ae6f.jpg"
+  },
+  {
+    "revision": "fa6f83c637baa2ea1cf21afe09e05947",
+    "url": "./static/media/BruceNPark1.fa6f83c6.jpg"
+  },
+  {
+    "revision": "de747c4b3355adf4c0611bd3fd90ca91",
+    "url": "./static/media/BruceNPark2.de747c4b.jpg"
+  },
+  {
+    "revision": "872a656ab76a318b668d3748b85feb6a",
+    "url": "./static/media/BruceNPark3.872a656a.jpg"
+  },
+  {
+    "revision": "649c9ee9339364f78de45ff464266f13",
+    "url": "./static/media/BruceNPark4.649c9ee9.jpg"
+  },
+  {
+    "revision": "bba365409a4dda192d41b2d9656dd313",
+    "url": "./static/media/LittleCove1.bba36540.jpg"
+  },
+  {
+    "revision": "c9dcf930f144d015a170a08d44b21e9b",
+    "url": "./static/media/LittleCove2.c9dcf930.jpg"
+  },
+  {
+    "revision": "c94e2d23a9f5e33d9aa4f7b9a990060d",
+    "url": "./static/media/LittleCove3.c94e2d23.jpg"
+  },
+  {
+    "revision": "1f4a3b4cb6b6d097e22cff9b9fcccd6e",
+    "url": "./static/media/LittleCove4.1f4a3b4c.jpg"
+  },
+  {
+    "revision": "1e1359422ca9ae62caf20d38708f42e5",
+    "url": "./static/media/SB1.1e135942.jpg"
+  },
+  {
+    "revision": "ef8939b577ac0b94f702d815efd2a5cf",
+    "url": "./static/media/SB2.ef8939b5.jpg"
+  },
+  {
+    "revision": "0a5b6525e2945221a52daed460143513",
+    "url": "./static/media/SB3.0a5b6525.jpg"
+  },
+  {
+    "revision": "ad3521ec4c65add108bb1b4c86c59c85",
+    "url": "./static/media/SB4.ad3521ec.jpg"
+  },
+  {
+    "revision": "96b203f9cfeb7bb34b581fd51d36bf93",
+    "url": "./static/media/Shipwrecks1.96b203f9.jpg"
+  },
+  {
+    "revision": "918d5cd6c09ef3a8609bcd0582e118f6",
+    "url": "./static/media/Shipwrecks2.918d5cd6.jpg"
+  },
+  {
+    "revision": "9b616646219d2efc17efeb9eafb121a0",
+    "url": "./static/media/Shipwrecks3.9b616646.jpg"
+  },
+  {
+    "revision": "0a3ac56cdb4ee6f99dc15b54e284a9d9",
+    "url": "./static/media/Shipwrecks4.0a3ac56c.jpg"
+  },
+  {
+    "revision": "0fd6c7ffa907552bba36dc69c414b71e",
+    "url": "./static/media/bg.0fd6c7ff.png"
+  }
+]);
