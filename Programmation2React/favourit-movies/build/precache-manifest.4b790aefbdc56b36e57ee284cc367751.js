553d54373268d86e02050affb9c8fb733e3cb440
martins81-web <ericmartins81@gmail.com>
2020-08-18 17:55:42 -0400

    upload

---
+++ Programmation2React/favourit-movies/build/precache-manifest.4b790aefbdc56b36e57ee284cc367751.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d947cc7855478c3b06b0ca1c2d290833",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c39571279e14b926a79c",
+    "url": "./static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "0a95bd6816d7bea36acd",
+    "url": "./static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "c39571279e14b926a79c",
+    "url": "./static/js/2.3d991c3e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "./static/js/2.3d991c3e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a95bd6816d7bea36acd",
+    "url": "./static/js/main.7427eff0.chunk.js"
+  },
+  {
+    "revision": "afe8d707910cb66b9155",
+    "url": "./static/js/runtime-main.ef7bf9b7.js"
+  },
+  {
+    "revision": "621e8f32dff8e1cced3d1b0ccbb2b58b",
+    "url": "./static/media/404.621e8f32.jpg"
+  }
+]);
