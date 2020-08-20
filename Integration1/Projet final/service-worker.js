6ec35dca81ee6867effba454c5d3e7fff4d8a692
martins81-web <ericmartins81@gmail.com>
2020-08-20 19:58:51 -0400

    update

---
+++ Integration1/Projet final/service-worker.js
@@ -0,0 +1,33 @@
+//Update cache names any time any of the cached files change.
+const CACHE_NAME = 'static-cache-v1';
+
+//Add list of files to cache here.
+const FILES_TO_CACHE = [
+    'offline.html',
+];
+
+
+
+self.addEventListener('install', (evt) => {
+    console.log('[ServiceWorker] Install');
+// Precache static resources here.
+    self.skipWaiting();
+});
+
+
+self.addEventListener('install', (evt) => {
+    console.log('[ServiceWorker] Install');
+// Precache static resources here.
+    evt.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            console.log('[ServiceWorker] Pre-caching offline page');
+            return cache.addAll(FILES_TO_CACHE);
+        })
+    );
+    self.skipWaiting();
+});
+
+self.addEventListener('fetch', (evt) => {
+    console.log('[ServiceWorker] Fetch', evt.request.url);
+//Add fetch event handler here.
+});
