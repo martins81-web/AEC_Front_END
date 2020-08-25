2dd4a90adb6cfd8520385f031e7d51ed8f6bfe86
martins81-web <ericmartins81@gmail.com>
2020-08-25 19:40:57 -0400

    pwa

---
+++ Programmation2React/hellopwa/build/precache-manifest.1c74f6f8fd00f97d25ca287b52365652.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c8b32165060ea3c3c262a30a8754bea",
+    "url": "./index.html"
+  },
+  {
+    "revision": "cc483f301a01868abba1",
+    "url": "./static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "af70b1cdc5b7fa27df67",
+    "url": "./static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "cc483f301a01868abba1",
+    "url": "./static/js/2.e23a9236.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "./static/js/2.e23a9236.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "af70b1cdc5b7fa27df67",
+    "url": "./static/js/main.a32e049e.chunk.js"
+  },
+  {
+    "revision": "596db6d2290197e71c87",
+    "url": "./static/js/runtime-main.cf705ba3.js"
+  },
+  {
+    "revision": "3abe438f0c273281baa9da6fa9fcc7d2",
+    "url": "./static/media/pwa.3abe438f.png"
+  }
+]);
--- Programmation2React/hellopwa/build/service-worker.js
+++ Programmation2React/hellopwa/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.26e4801e6ee4f035714e20ae111dc104.js"
+  "./precache-manifest.1c74f6f8fd00f97d25ca287b52365652.js"
 );
 
 self.addEventListener('message', (event) => {
