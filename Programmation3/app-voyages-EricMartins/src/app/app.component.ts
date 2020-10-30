9562453b8e30baf2a7792f6f3cf5d5457e728618
Eric <eric@marketingauto.ca>
2020-10-29 22:17:41 -0400

    update

--- Programmation3/app-voyages-EricMartins/src/app/app.component.ts
+++ Programmation3/app-voyages-EricMartins/src/app/app.component.ts
@@ -3,6 +3,12 @@ import { Component, Input } from '@angular/core';
 import { Forfait } from './forfait';
 import { forfaits } from './mock-forfaits';
 
+export interface Tile {
+  color: string;
+  cols: number;
+  rows: number;
+  text: string;
+}
 
 @Component({
   selector: 'app-root',
@@ -12,6 +18,12 @@ import { forfaits } from './mock-forfaits';
 export class AppComponent {
   title = 'app-voyages-EricMartins';
   mesForfaits: Array<Forfait> = forfaits;
+  tiles: Tile[] = [
+    {text: 'One', cols: 3, rows: 1, color: 'lightblue'},
+    {text: 'Two', cols: 1, rows: 2, color: 'lightgreen'},
+    {text: 'Three', cols: 1, rows: 1, color: 'lightpink'},
+    {text: 'Four', cols: 2, rows: 1, color: '#DDBDF1'},
+  ];
   ngOnInit(): void {
     console.log('Mes forfaits',this.mesForfaits);
   }
