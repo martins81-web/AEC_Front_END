de52ea6474a4744cf9a618a33d164ec942a5753e
Eric <eric@marketingauto.ca>
2020-11-05 23:04:06 -0500

    update

--- Programmation3/app-voyages-EricMartins/src/app/app.module.ts
+++ Programmation3/app-voyages-EricMartins/src/app/app.module.ts
@@ -1,6 +1,12 @@
 import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatGridListModule } from '@angular/material/grid-list';
+import { MatInputModule } from '@angular/material/input';
+import { MatSliderModule } from '@angular/material/slider';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
@@ -8,6 +14,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ForfaitCompletComponent } from './forfait-complet/forfait-complet.component';
 import { ForfaitComponent } from './forfait/forfait.component';
+import { FormulaireForfaitComponent } from './formulaire-forfait/formulaire-forfait.component';
 import { ListeForfaitsComponent } from './liste-forfaits/liste-forfaits.component';
 import { MiniForfaitComponent } from './mini-forfait/mini-forfait.component';
 
@@ -17,13 +24,22 @@ import { MiniForfaitComponent } from './mini-forfait/mini-forfait.component';
     ListeForfaitsComponent,
     ForfaitComponent,
     MiniForfaitComponent,
-    ForfaitCompletComponent
+    ForfaitCompletComponent,
+    FormulaireForfaitComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatGridListModule,MatCardModule,
+    MatGridListModule,
+    MatCardModule,
+    MatFormFieldModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatAutocompleteModule,
+    MatButtonModule,
+    MatInputModule,
+    MatSliderModule
   ],
   providers: [],
   bootstrap: [AppComponent]
